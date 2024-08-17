0648ac483692300669dce9034b4a7e846db5bd92
Andrew <49104171+carsonak@users.noreply.github.com>
2024-08-17 17:39:36 +0300

    Fixed array_to_bst when duplicate values in array

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,4 +1,25 @@
 #include "binary_trees.h"
+
+/**
+ * bstsearch - searches a binary search tree for value.
+ * @tree: pointer to the root node of the tree.
+ * @value: the value to search for.
+ *
+ * Return: pointer to the node with the value, NULL otherwise.
+ */
+static bst_t *bstsearch(const bst_t *tree, int value)
+{
+	while (tree && tree->n != value)
+	{
+		if (value < tree->n)
+			tree = tree->left;
+		else if (value > tree->n)
+			tree = tree->right;
+	}
+
+	return ((bst_t *)tree);
+}
+
 /**
  * btree_delete - free a binary tree data structure.
  * @tree: root node of the tree.
@@ -30,7 +51,7 @@ bst_t *array_to_bst(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		if (!bst_insert(&tree, array[i]))
+		if (!bst_insert(&tree, array[i]) && !bstsearch(tree, array[i]))
 		{
 			btree_delete(tree);
 			return (NULL);
